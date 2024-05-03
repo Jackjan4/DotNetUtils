0c13468e70102d6355b42b0bc3f0b791a7f4ebe8
JanR <Jackjan@protonmail.com>
2024-05-03 13:47:46 +0200

    Renamed library & deleted GetLocalIpv4 because "local" isn't really defined

--- NETUtils/Net/NetworkUtils.cs
+++ NETUtils/Net/NetworkUtils.cs
@@ -12,41 +12,12 @@ namespace De.JanRoslan.NETUtils.Net {
 
 
         /// <summary>
-        /// Returns the local IPv4 address as string.
-        /// (The method is taken from Bibliothek.dll from Brandgroup (Volker Niemeyer), cleaned up and converted to C#)
-        /// </summary>
-        /// <returns>Returns the first local Ipv4 address that is found</returns>
-        public static string GetLocalIpv4() {
-            string result = null;
-            List<string> ipAddresses = new List<string>();
-
-            // Accessing network can cause exceptions. Caller of this method has to handle them.
-
-            int i = 0;
-            foreach (IPAddress ipAddr in Dns.GetHostAddresses(Dns.GetHostName())) {
-                string currentIpAddr = Dns.GetHostEntry(Dns.GetHostName()).AddressList[i].ToString();
-
-                if (!currentIpAddr.Contains(":"))
-                    ipAddresses.Add(currentIpAddr);
-                i += 1;
-            }
-
-            if (ipAddresses.Count > 0)
-                result = ipAddresses[0];
-
-            return result;
-        }
-
-
-
-        /// <summary>
         /// Enumerates all network interfaces and gets their unicast addresses
         /// (The method is taken from Bibliothek.dll from Brandgroup (Volker Niemeyer), cleaned up and converted to C#)
         /// </summary>
         /// <param name="localIp"></param>
         /// <returns></returns>
-        public static List<string> GetAllIpAddresses(out string localIp) {
-            localIp = null;
+        public static List<string> GetAllIpAddresses() {
             List<string> result = new List<string>();
 
             // Accessing network can cause exceptions. Caller of this method has to handle them.
@@ -55,10 +26,6 @@ namespace De.JanRoslan.NETUtils.Net {
                 if (netInterface.OperationalStatus == OperationalStatus.Up && netInterface.NetworkInterfaceType != NetworkInterfaceType.Loopback) {
                     foreach (UnicastIPAddressInformation ipAddr in netInterface.GetIPProperties().UnicastAddresses) {
                         if (ipAddr.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork) {
-
-                            if (!ipAddr.Address.ToString().StartsWith("192.168")) {
-                                localIp = ipAddr.Address.ToString();
-                            }
                             result.Add(ipAddr.Address.ToString());
                         }
                     }
