2cc1ccfd4af54c2b7f2e71e3392c880caf9e2f05
J R <Jackjan@protonmail.com>
2024-08-28 13:04:12 +0200

    Changed InitializeableService classes to interfaces

---
+++ DotNETUtils/Mvvm/IInitializableService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Roslan.DotNetUtils.Mvvm {
+    public interface IInitializableService {
+
+
+
+        /// <summary>
+        /// Determines whether the service has been initialized.
+        /// </summary>
+        public bool IsInitialized { get; }
+
+
+
+        public void Initialize();
+
+
+
+        public void Deinitialize();
+    }
+}
---
+++ DotNETUtils/Mvvm/IInitializableServiceAsync.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Roslan.DotNetUtils.Mvvm {
+    public interface IInitializableServiceAsync {
+
+
+
+        /// <summary>
+        /// Determines whether the service has been initialized.
+        /// </summary>
+        public bool IsInitialized { get; set; }
+
+
+
+        public Task InitializeAsync();
+
+
+
+        public Task DeInitializeAsync();
+    }
+}
