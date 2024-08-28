1973aef6934d246a924c4a7314bfb505e4ee8a1c
J R <Jackjan@protonmail.com>
2024-08-28 12:06:18 +0200

    Added InitializableService & InitializableServiceAsync

---
+++ DotNETUtils/Mvvm/InitializableService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Roslan.DotNetUtils.Mvvm {
+    public abstract class InitializableService {
+
+
+
+        /// <summary>
+        /// Determines whether the service has been initialized.
+        /// </summary>
+        public bool IsInitialized { get; private set; }
+
+        public void Initialize() {
+            if (IsInitialized)
+                return;
+
+            InitializeService();
+
+            IsInitialized = true;
+        }
+
+
+
+        protected abstract void InitializeService();
+        protected abstract void DeinitializeService();
+
+
+
+        public void DeInitialize() {
+            if (IsInitialized)
+                DeinitializeService();
+            IsInitialized = false;
+        }
+    }
+}
---
+++ DotNETUtils/Mvvm/InitializableServiceAsync.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Roslan.DotNetUtils.Mvvm {
+    public abstract class InitializableServiceAsync {
+
+
+
+        /// <summary>
+        /// Determines whether the service has been initialized.
+        /// </summary>
+        public bool IsInitialized { get; private set; }
+
+        public async Task InitializeAsync() {
+            if (IsInitialized)
+                return;
+
+            await InitializeServiceAsync();
+
+            IsInitialized = true;
+        }
+
+
+
+        protected abstract Task InitializeServiceAsync();
+        protected abstract Task DeinitializeServiceAsync();
+
+
+        public async Task DeInitializeAsync() {
+            if (IsInitialized)
+                await DeinitializeServiceAsync();
+            IsInitialized = false;
+        }
+    }
+}
